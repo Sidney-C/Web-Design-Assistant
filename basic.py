cf1abdd12c50e12cc9cdc5cf6455f148673eba35
Sidney-C <158471670+Sidney-C@users.noreply.github.com>
2024-02-05 13:53:36 +0000

    simple one page form, now working

--- basic.py
+++ basic.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, session
 from flask_wtf import FlaskForm
-from wtforms import StringField, RadioField, SubmitField, validators
+from wtforms import TextAreaField, StringField, RadioField, SubmitField, validators
 
 app = Flask(__name__)
 
@@ -8,34 +8,28 @@ app.config['SECRET_KEY'] = 'secret'
 
 class NameForm(FlaskForm):
     sitename = StringField('Enter the name of your website', [validators.DataRequired()])
-    textcontent = StringField('Enter the text for your website', [validators.DataRequired()])
-    submit = SubmitField('Submit')
+    textcontent = TextAreaField('Enter the text for your website', [validators.DataRequired()])
+    submit = SubmitField('Generate Website!')
     
 @app.route('/', methods = ['GET', 'POST'])
 def index():
-    step = request.args.get('step', default = 1, type = int)
     sitename = False
     textcontent = False
     nameform = NameForm()
 
     if nameform.validate_on_submit():
-        if step == 1:
-            sitename = nameform.sitename.data
-            nameform.sitename.data = ''
-            print(f"Step 1: {sitename}")
-        elif step == 2:
-            textcontent = nameform.textcontent.data
-            nameform.textcontent.data = ''
-            print(f"Step 2: {textcontent}")
-        step += 1
-            #return redirect(url_for('yourwebsite'))
+        session['sitename'] = nameform.sitename.data
+        nameform.sitename.data = ''
+        #print(f"Step 1: {sitename}")
+        session['textcontent'] = nameform.textcontent.data
+        nameform.textcontent.data = ''
+        #print(f"Step 2: {textcontent}")
+        return redirect(url_for('yourwebsite'))
 
-    return render_template('index.html', nameform=nameform, sitename=sitename, textcontent=textcontent, step=step)
+    return render_template('index.html', nameform=nameform)
 
 @app.route('/yourwebsite')
 def yourwebsite():
-    #sitename = sitename
-    #textcontent = textcontent
 
     return render_template('yourwebsite.html')
 
