4d0267d1b2d5cadc1dd0bd288e49209c0689f21a
Sidney-C <158471670+Sidney-C@users.noreply.github.com>
2024-02-24 20:35:53 +0000

    now supports unlimited number of paragraphs

--- forms.py
+++ forms.py
@@ -16,7 +16,8 @@ submittext = 'Submit'
 pagenametext = '''To get started, enter the name of this page. The name
 you enter will appear at the top of the page.'''
 urlnametext = '''Enter the URL for this page. This will appear at the end of it's address, so I recommend you make it short and simple.'''
-pagetexttext = '''Enter the text for the first section of this page. Don't worry about fitting everything in, as you will have the option to add another section soon'''
+pagetexttext = '''Enter the text for the current section of this page. Don't worry about fitting everything in, as you will have the option to add another section soon'''
+newsectiontext = '''Would you like to add another section to your page?'''
 
 class PageName(FlaskForm):
     userinput = StringField(pagenametext, [validators.DataRequired()])
@@ -28,9 +29,13 @@ class URLName(FlaskForm):
 
 class PageText(FlaskForm):
     userinput = TextAreaField(pagetexttext, [validators.DataRequired()])
-    submit= SubmitField(submittext)
+    submit = SubmitField(submittext)
 
-formfields = [PageName, URLName, PageText]
+class NewSection(FlaskForm):
+    userinput = RadioField(newsectiontext, choices=[('Yes', 'Yes'), ('No', 'No')])
+    submit = SubmitField(submittext)
+
+formfields = [PageName, URLName, PageText, NewSection]
 
 @app.route('/testpage1', methods = ['GET', 'POST'])
 def testpage1():
@@ -42,11 +47,17 @@ def testpage1():
     
     if chatbox.validate_on_submit():
         assetlist.append(chatbox.userinput.data)
-        print(assetlist)
-        session['i'] += 1
-        print(session['i'])
         
-        if session['i'] > 2:
+        if session['i'] == 3:
+            if chatbox.userinput.data == 'Yes':
+                session['i'] = 1
+            else:
+                stopnum = assetlist.index('No', -1)
+                session['stopnum'] = stopnum
+            assetlist.pop(-1)
+                
+        session['i'] += 1
+        if session['i'] > 3:
             return redirect(url_for('testpage2'))
         
         chatbox = formfields[session['i']]()
@@ -57,7 +68,12 @@ def testpage1():
 @app.route('/testpage2')
 def testpage2():
 
-    return render_template('testpage2.html')
+    stopnum = session.get('stopnum')
+
+    print(assetlist)
+    print(stopnum)
+    
+    return render_template('testpage2.html', assetlist = assetlist, stopnum = stopnum)
 
 if __name__ == '__main__':
     app.run(debug = True)
