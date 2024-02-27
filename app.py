03d2a4907e5ce8c5e159e1ad9d0954be28fd2e5f
Sidney-C <158471670+Sidney-C@users.noreply.github.com>
2024-02-27 21:39:20 +0000

    can make multiple pages

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ imagecount = 1
 navbarlinks = {}
 sitenames = []
 formfields = [PageName, URLName, PageText, PageImage, NewSection, AddToNavbar]
+websitename = ''
 
 def checkextension(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
@@ -25,56 +26,70 @@ def checkextension(filename):
 def testpage1():
     
     global imagecount
-    
+    global websitename
+    global assetlist
+
     if 'i' not in session:
         session['i'] = 0
 
-    chatbox = formfields[session['i']]()
-    
-    if chatbox.validate_on_submit():
+    if websitename == '':
+        chatbox = WebsiteName()
+        if chatbox.validate_on_submit():
+            websitename = chatbox.userinput.data
+            chatbox = formfields[session['i']]()
+            chatbox.userinput.data = ''
 
-        if session['i'] == 2:
-            assetlist.append([chatbox.userinput.data])
-
-        elif session['i'] == 3:
-            session['image'] = chatbox.userinput.data
-            if session['image'] and checkextension(session['image'].filename):
-                filename = 'user_image' + str(imagecount)
-                imagecount += 1
-                filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-                session['image'].save(filepath)
-                assetlist[-1].append(filename)
-                chatbox.userinput.data = ''
-                session.pop('image', None)
-            else:
-                assetlist[-1].append("no file uploaded")
-                chatbox.userinput.data = ''
-                session.pop('image', None)
-        
-        else:
-            assetlist.append(chatbox.userinput.data)
-
-        if session['i'] == 4:
-            if chatbox.userinput.data == 'Yes':
-                session['i'] = 1
-            else:
-                stopnum = assetlist.index('No', -1)
-                session['stopnum'] = stopnum
-            assetlist.pop(-1)
-
-        if session['i'] == 5:
-            if chatbox.userinput.data == 'Yes':
-                navbarlinks[assetlist[0]] = (url_for('currentpage', currenturl=assetlist[1]))
-                sitenames.append(assetlist[0])
-            assetlist.pop(-1)
-                
-        session['i'] += 1
-        if session['i'] == len(formfields):
-            allassets[assetlist[1]] = assetlist
-            return redirect(url_for('currentpage', currenturl = assetlist[1]))
-        
+    else:
         chatbox = formfields[session['i']]()
-        chatbox.userinput.data = ''
+        
+        if chatbox.validate_on_submit():
+
+            if session['i'] == 2:
+                assetlist.append([chatbox.userinput.data])
+
+            elif session['i'] == 3:
+                session['image'] = chatbox.userinput.data
+                if session['image'] and checkextension(session['image'].filename):
+                    filename = 'user_image' + str(imagecount)
+                    imagecount += 1
+                    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                    session['image'].save(filepath)
+                    assetlist[-1].append(filename)
+                    chatbox.userinput.data = ''
+                    session.pop('image', None)
+                else:
+                    assetlist[-1].append("no file uploaded")
+                    chatbox.userinput.data = ''
+                    session.pop('image', None)
+            
+            else:
+                assetlist.append(chatbox.userinput.data)
+
+            if session['i'] == 4:
+                if chatbox.userinput.data == 'Yes':
+                    session['i'] = 1
+                else:
+                    stopnum = assetlist.index('No', -1)
+                    session['stopnum'] = stopnum
+                assetlist.pop(-1)
+
+            if session['i'] == 5:
+                if chatbox.userinput.data == 'Yes':
+                    navbarlinks[assetlist[0]] = (url_for('currentpage', currenturl=assetlist[1]))
+                    sitenames.append(assetlist[0])
+                assetlist.pop(-1)
+                    
+            session['i'] += 1
+            if session['i'] == len(formfields):
+                assetlist.append(session['stopnum'])
+                allassets[assetlist[1]] = assetlist
+                currenturl = assetlist[1]
+                assetlist = []
+                session['i'] = 0
+                return redirect(url_for('currentpage', currenturl = currenturl))
+            
+            chatbox = formfields[session['i']]()
+            chatbox.userinput.data = ''
 
     return render_template('testpage1.html', chatbox = chatbox)
 
@@ -89,18 +104,38 @@ def testpage2():
         #filenames.append(assetlist[i][1])
     
     print(allassets)
-    return render_template('testpage2.html', assetlist = assetlist, stopnum = stopnum)
+    return render_template('testpage2.html', assetlist = assetlist, stopnum = stopnum, websitename = websitename)
 
 @app.route('/testpage2/<currenturl>')
 def currentpage(currenturl):
 
-    stopnum = session.get('stopnum')
-    currentassets = allassets[currenturl]
 
+    currentassets = allassets[currenturl]
+    stopnum = currentassets[-1]
+
+    print('navbarlinks:')
     print(navbarlinks)
+    print('sitenames:')
     print(sitenames)
+    print('websitename:')
+    print(websitename)
+    print('allassets:')
+    print(allassets)
+    print('currentassets:')
+    print(currentassets)
+    print('stopnum:')
+    print(stopnum)
     
-    return render_template('testpage2.html', currentassets = currentassets, stopnum = stopnum)
+    return render_template('testpage2.html', currentassets = currentassets, stopnum = stopnum, websitename = websitename)
+
+@app.route('/testpage3')
+def testpage3():
+
+    findbrowser = request.user_agent.string
+    browser = parse(findbrowser)
+    browsername = browser.browser.family
+
+    return render_template('testpage3.html', browsername=browsername)
 
 if __name__ == '__main__':
     app.run(debug = True)
--- forms.py
+++ forms.py
@@ -3,12 +3,17 @@ from flask_wtf import FlaskForm
 from wtforms import TextAreaField, StringField, RadioField, SubmitField, FileField, validators
 
 submittext = 'Submit'
-pagenametext = '''To get started, enter the name of this page. The name you enter will appear at the top of the page.'''
+websitenametext = '''Hi there! To get started, enter the name of your website. Keep in mind that this will appear at the top of every page.'''
+pagenametext = '''Now, enter the name of this page. The name you enter will appear at the top of the page.'''
 urlnametext = '''Enter the URL for this page. This will appear at the end of it's address, so I recommend you make it short and simple.'''
 pagetexttext = '''Enter the text for the current section of this page. Don't worry about fitting everything in, as you will have the option to add another section soon'''
 pageimagetext = '''If you'd like to add an image, upload it here using the "Browse" button. If you don't want an image, just click "Submit" without uploading anything.'''
 newsectiontext = '''Would you like to add another section to your page?'''
 addtonavbartext = '''Would you like to add a link to this page to the navigation bar at the top of the site?'''
+
+class WebsiteName(FlaskForm):
+    userinput = StringField(websitenametext, [validators.DataRequired()])
+    submit = SubmitField(submittext)
 
 class PageName(FlaskForm):
     userinput = StringField(pagenametext, [validators.DataRequired()])
