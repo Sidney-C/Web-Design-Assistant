a73fb94bb6108bb3dfe8edcd9a78def0099b5069
Sidney-C <158471670+Sidney-C@users.noreply.github.com>
2024-02-29 19:29:14 +0000

    can choose theme

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask_wtf import FlaskForm
 from wtforms import TextAreaField, StringField, RadioField, SubmitField, FileField, validators
 from user_agents import parse
 from forms import *
-#from chatoptions import options
 import os
 
 app = Flask(__name__)
@@ -18,6 +17,8 @@ imagecount = 1
 navbarlinks = {}
 sitenames = []
 formfields = [PageName, URLName, PageText, PageImage, NewSection, AddToNavbar]
+starterfields = [WebsiteName, WebsiteTheme]
+starterassets = []
 websitename = ''
 websitetheme = ''
 stopnum = 0
@@ -36,9 +37,37 @@ def welcome():
     launchapp = LaunchApp()
 
     if launchapp.validate_on_submit():
-        return redirect(url_for('chat'))
+        return redirect(url_for('choosename'))
 
     return render_template('welcome.html', launchapp = launchapp)
+
+@app.route('/choosename', methods = ['GET', 'POST'])
+def choosename():
+    
+    global websitename
+
+    chatbox = WebsiteName()
+
+    if chatbox.validate_on_submit():
+        websitename = chatbox.userinput.data
+        chatbox.userinput.data = ''
+        return redirect(url_for('choosetheme'))
+
+    return render_template('chat.html', chatbox = chatbox)
+
+@app.route('/choosetheme', methods = ['GET', 'POST'])
+def choosetheme():
+    
+    global websitetheme
+
+    chatbox = WebsiteTheme()
+
+    if chatbox.validate_on_submit():
+        websitetheme = chatbox.userinput.data
+        chatbox.userinput.data = ''
+        return redirect(url_for('chat'))
+
+    return render_template('chat.html', chatbox = chatbox)
 
 @app.route('/chat', methods = ['GET', 'POST'])
 def chat():
@@ -52,71 +81,56 @@ def chat():
     if 'i' not in session:
         session['i'] = 0
 
-    if websitename == '':
-        chatbox = WebsiteName()
-        if chatbox.validate_on_submit():
-            websitename = chatbox.userinput.data
-            #chatbox = WebsiteTheme()
-            chatbox = formfields[session['i']]()
-            chatbox.userinput.data = ''
+    chatbox = formfields[session['i']]()
 
-    #if websitetheme == '':
-        #if chatbox.validate_on_submit():
-            #websitetheme = chatbox.userinput.data
-            #chatbox = formfields[session['i']]()
-            #chatbox.userinput.data = ''
+    if chatbox.validate_on_submit():
 
-    else:
-        chatbox = formfields[session['i']]()
-        
-        if chatbox.validate_on_submit():
+        if session['i'] == 2:
+            assetlist.append([chatbox.userinput.data])
 
-            if session['i'] == 2:
-                assetlist.append([chatbox.userinput.data])
-
-            elif session['i'] == 3:
-                session['image'] = chatbox.userinput.data
-                if session['image'] and checkextension(session['image'].filename):
-                    filename = 'user_image' + str(imagecount)
-                    imagecount += 1
-                    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-                    session['image'].save(filepath)
-                    assetlist[-1].append(filename)
-                    chatbox.userinput.data = ''
-                    session.pop('image', None)
-                else:
-                    assetlist[-1].append("no file uploaded")
-                    chatbox.userinput.data = ''
-                    session.pop('image', None)
-            
+        elif session['i'] == 3:
+            session['image'] = chatbox.userinput.data
+            if session['image'] and checkextension(session['image'].filename):
+                filename = 'user_image' + str(imagecount)
+                imagecount += 1
+                filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                session['image'].save(filepath)
+                assetlist[-1].append(filename)
+                chatbox.userinput.data = ''
+                session.pop('image', None)
             else:
-                assetlist.append(chatbox.userinput.data)
+                assetlist[-1].append("no file uploaded")
+                chatbox.userinput.data = ''
+                session.pop('image', None)
+        
+        else:
+            assetlist.append(chatbox.userinput.data)
 
-            if session['i'] == 4:
-                if chatbox.userinput.data == 'Yes':
-                    session['i'] = 1
-                else:
-                    stopnum = assetlist.index('No', -1)
-                    session['stopnum'] = stopnum
-                assetlist.pop(-1)
+        if session['i'] == 4:
+            if chatbox.userinput.data == 'Yes':
+                session['i'] = 1
+            else:
+                stopnum = assetlist.index('No', -1)
+                session['stopnum'] = stopnum
+            assetlist.pop(-1)
 
-            if session['i'] == 5:
-                if chatbox.userinput.data == 'Yes':
-                    navbarlinks[assetlist[0]] = (url_for('currentpage', currenturl=assetlist[1]))
-                    sitenames.append(assetlist[0])
-                assetlist.pop(-1)
-                    
-            session['i'] += 1
-            if session['i'] == len(formfields):
-                assetlist.append(session['stopnum'])
-                allassets[assetlist[1]] = assetlist
-                currenturl = assetlist[1]
-                assetlist = []
-                session['i'] = 0
-                return redirect(url_for('currentpage', currenturl = currenturl))
-            
-            chatbox = formfields[session['i']]()
-            chatbox.userinput.data = ''
+        if session['i'] == 5:
+            if chatbox.userinput.data == 'Yes':
+                navbarlinks[assetlist[0]] = (url_for('currentpage', currenturl=assetlist[1]))
+                sitenames.append(assetlist[0])
+            assetlist.pop(-1)
+                
+        session['i'] += 1
+        if session['i'] == len(formfields):
+            assetlist.append(session['stopnum'])
+            allassets[assetlist[1]] = assetlist
+            currenturl = assetlist[1]
+            assetlist = []
+            session['i'] = 0
+            return redirect(url_for('currentpage', currenturl = currenturl))
+        
+        chatbox = formfields[session['i']]()
+        chatbox.userinput.data = ''
 
     return render_template('chat.html', chatbox = chatbox)
 
@@ -131,12 +145,12 @@ def yourwebsite():
 @app.route('/yourwebsite/<currenturl>')
 def currentpage(currenturl):
 
-
+    print(websitetheme)
+    print(websitename)
     currentassets = allassets[currenturl]
     stopnum = currentassets[-1]
-    print(sitenames)
     
-    return render_template('yourwebsite.html', currentassets = currentassets, stopnum = stopnum, websitename = websitename, navbarlinks = navbarlinks, sitenames = sitenames)
+    return render_template('yourwebsite.html', websitename = websitename, websitetheme = websitetheme, currentassets = currentassets, stopnum = stopnum, navbarlinks = navbarlinks, sitenames = sitenames)
 
 @app.route('/showsource')
 def showsource():
